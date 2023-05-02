59dd06d4f8f4b8d5dfbfc1c3155e6bf1b7a0e856
Fiona Duvivier <fiona.duvivier@hotmail.com>
2023-05-02 15:05:43 +0200

    Script.js

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,69 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+window.addEventListener('load', function () {
+	let boutonG = document.getElementById("fleche_gauche");
+	let boutonD = document.getElementById("fleche_droite");
+	boutonG.addEventListener('click', onClickLeft);
+	boutonD.addEventListener('click', onClickRight);
+})
+
+
+const dotContainer = document.getElementById("js-dots");
+let dotsArray = [];
+
+for (let i = 0; i <= slides.length - 1; i++) {
+	dotsArray[i] = document.createElement("span");
+	dotsArray[i].classList.add("dot");
+	dotContainer.appendChild(dotsArray[i]);
+}
+
+
+dotsArray[0].classList.add("dot_selected");
+let currentDot = 0;
+
+function onClickRight() {
+	if (currentDot < 3) {
+		dotsArray[currentDot].classList.remove("dot_selected");
+		dotsArray[currentDot + 1].classList.add("dot_selected");
+		currentDot++;
+	}
+	else {
+		dotsArray[currentDot].classList.remove("dot_selected");
+		dotsArray[0].classList.add("dot_selected");
+		currentDot = 0;
+	}
+	alert("Bouton cliqué");
+}
+
+function onClickLeft() {
+	if (currentDot > 0) {
+		dotsArray[currentDot].classList.remove("dot_selected");
+		dotsArray[currentDot - 1].classList.add("dot_selected");
+		currentDot--;
+	}
+	else {
+		dotsArray[currentDot].classList.remove("dot_selected");
+		dotsArray[3].classList.add("dot_selected");
+		currentDot = 3;
+	}
+}
+
+
